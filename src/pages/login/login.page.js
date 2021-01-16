3a20bf0349f1536ed7801b489d359212ececdd99
Pera <alex96pro@gmail.com>
2021-01-16 01:50:10 +0100

    centered all forms

--- src/pages/login/login.page.js
+++ src/pages/login/login.page.js
@@ -31,23 +31,24 @@ export default function Login() {
     return (
         <div className="login">
             <NavBar loggedIn={false}/>
-            <div className="login-header">Hungry ?</div>
-            <div className="wrapper-container-big">
-                <form onSubmit={handleSubmit(login)}>
-                    <div className="label-accent-color">Email</div>
-                    <input type="email" name="email" ref={register({required:true})}/>
-                    {errors.email && <p className="message-danger">Email is required</p>}
-                    <div className="label-accent-color">Password</div>
-                    <input type="password" name="password" ref={register({required:true})}/>
-                    {errors.password && <p className="message-danger">Password is required</p>}
-                    <button type="submit" className="button-long">{loadingStatus ? <img src={Loader} className="loader-small" alt="Loading..."/> : "Log in"}</button>
-                </form>
-                {logInMessage && <p className="message-danger">{logInMessage}</p>}
-                {history.location.message && <p className="message-success">{history.location.message}</p>}
+            <div className="login-container">
+                <div className="form-container">
+                    <div className="login-header">Log in to your account</div>
+                    <form onSubmit={handleSubmit(login)}>
+                        <div className="label-accent-color">Email</div>
+                        <input type="email" name="email" ref={register({required:true})}/>
+                        {errors.email && <p className="message-danger">Email is required</p>}
+                        <div className="label-accent-color">Password</div>
+                        <input type="password" name="password" ref={register({required:true})}/>
+                        {errors.password && <p className="message-danger">Password is required</p>}
+                        <button type="submit" className="button-long">{loadingStatus ? <img src={Loader} className="loader-small" alt="Loading..."/> : "Log in"}</button>
+                    </form>
+                    {logInMessage && <p className="message-danger">{logInMessage}</p>}
+                    {history.location.message && <p className="message-success">{history.location.message}</p>}
+                </div>
+                <div><button type="button" onClick={() => setShowModal(true)} className="button-link">Forgot password?</button></div>
             </div>
-            <div><button type="button" onClick={() => setShowModal(true)} className="button-link">Forgot password?</button></div>
             {showModal && <ForgottenPasswordModal closeModal={closeModal}/>}
-            
         </div>
     );
 }
