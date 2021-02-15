26009e84228ca786c30d436b7715259d6d1bdc8e
Pera <alex96pro@gmail.com>
2021-02-15 20:38:15 +0100

    fixed google maps reload

--- src/components/common/google-autocomplete.js
+++ src/components/common/google-autocomplete.js
@@ -4,8 +4,18 @@ export default function GoogleAutocomplete(props) {
     const initMap = window.initMap;
     useEffect(() => {
         const googleMapsScript = document.getElementById('google-maps-script');
-        if(googleMapsScript){
-            // if google maps script is loaded that means that user redirected to page where autocomplete is needed
+        if(!googleMapsScript){ //scripts not loaded
+            const script1 = document.createElement('script');
+            script1.src = "./initMap.js";
+            document.body.appendChild(script1);
+            script1.onload = () => {
+                const script2 = document.createElement('script');
+                script2.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_API_KEY}&callback=initMap&libraries=places&v=weekly`;
+                script2.id = 'google-maps-script';
+                document.body.appendChild(script2);
+            }
+        }else{
+            // if google maps scripts are loaded that means that user redirected to page where autocomplete is needed
             // in that case for some reason initMap function needs to be called again.
             // initMap always puts one div with className pac-container so we need to remove it because we only need 1 in order for autocomplete to work properly
             initMap();
